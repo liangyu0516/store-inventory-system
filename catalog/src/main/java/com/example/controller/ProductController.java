63952ad18699740f2410aabb83845db555537f47
Liang-Yu Chen <cy9815526@gmail.com>
2024-10-21 21:44:50 -0400

    feat: set up the catalog service

---
+++ catalog/src/main/java/com/example/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.example.controller;
+
+import com.example.model.Product;
+import com.example.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping("/products/{productName}")
+    public Product getProduct(@PathVariable String productName) {
+        return productService.getProduct(productName);
+    }
+}

21f42f37e7e0b76e290ac01fd4292cd273121032
Liang-Yu Chen <cy9815526@gmail.com>
2024-10-21 22:28:14 -0400

    feat: modify frontend service to request product information from catalog service

--- frontend/src/main/java/com/example/controller/ProductController.java
+++ frontend/src/main/java/com/example/controller/ProductController.java
@@ -6,9 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class ProductController {
+
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Autowired
     private ProductService productService;
--- frontend/src/main/java/com/example/service/ProductService.java
+++ frontend/src/main/java/com/example/service/ProductService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,25 +13,16 @@ import java.util.List;
 @RestController
 public class ProductService {
 
-    private List<Product> products = new ArrayList<>();
+    private final RestTemplate restTemplate;
 
-    public ProductService() {
-        products.add(new Product("Tux", 6.9, 100));
-        products.add(new Product("Uno", 5.0, 100));
-        products.add(new Product("Clue", 15.0, 100));
-        products.add(new Product("Lego", 23.3, 100));
-        products.add(new Product("Chess", 17.5, 100));
-        products.add(new Product("Barbie", 10.0, 100));
-        products.add(new Product("Bubbles", 2.75, 100));
-        products.add(new Product("Frisbee", 8.8, 100));
-        products.add(new Product("Twister", 13.3, 100));
-        products.add(new Product("Elephant", 20.0, 100));
+    @Autowired
+    public ProductService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
     }
 
     public Product getProduct(@PathVariable String productName) {
-        return products.stream()
-            .filter(product -> product.getName().equalsIgnoreCase(productName))
-            .findFirst()
-            .orElse(null);
+        String getProductUrl = "http://localhost:5001/products/" + productName;
+        Product product = restTemplate.getForObject(getProductUrl, Product.class);
+        return product;
     }
 }

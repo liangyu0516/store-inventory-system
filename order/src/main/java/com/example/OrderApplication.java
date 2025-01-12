4f8585fadee56e7baf89b05acffa6be091be515a
Liang-Yu Chen <cy9815526@gmail.com>
2025-01-11 23:14:34 -0500

    feat: init order service

--- order/src/main/java/com/example/OrderApplication.java
+++ order/src/main/java/com/example/OrderApplication.java
@@ -2,14 +2,15 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-public class FrontendApplication {
+public class OrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FrontendApplication.class, args);
+        SpringApplication.run(OrderApplication.class, args);
     }
 
     @Bean
